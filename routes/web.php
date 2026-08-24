0406a8d5f89315055cb4916074d71074bb27dca2
Tarique Code <tariquecode@gmail.com>
2026-08-24 13:43:51 +0600

    Add protected production health route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\AuditLogController;
 use App\Http\Controllers\Admin\CmsController;
 use App\Http\Controllers\Admin\DocumentController;
+use App\Http\Controllers\Admin\HealthController;
 use App\Http\Controllers\Admin\InfrastructureController;
 use App\Http\Controllers\Admin\SettingsController;
 use App\Http\Controllers\Admin\UserController;
@@ -17,7 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', fn () => view('welcome'))->name('home');
 Route::get('/pages/{slug}', [CmsPageController::class, 'show'])->name('cms.page');
 Route::middleware('guest')->group(function () { Route::get('/login', [AuthController::class, 'showLogin'])->name('login'); Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:6,1')->name('login.store'); });
-
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', DashboardController::class)->name('dashboard')->middleware('permission:dashboard.view');
     Route::middleware('role:super-admin,administrator,project-manager,support-agent')->prefix('admin')->group(function () {
@@ -26,6 +26,7 @@ Route::middleware('auth')->group(function () {
         Route::middleware('permission:users.manage')->group(function () { Route::get('/users/create', [UserController::class, 'create'])->name('admin.users.create'); Route::post('/users', [UserController::class, 'store'])->name('admin.users.store'); Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('admin.users.edit'); Route::patch('/users/{user}', [UserController::class, 'update'])->name('admin.users.update'); Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('admin.users.destroy'); });
     });
     Route::prefix('admin')->middleware('permission:audit.view')->group(function () { Route::get('/audit', [AuditLogController::class, 'index'])->name('admin.audit'); });
+    Route::prefix('admin')->middleware('permission:health.view')->group(function () { Route::get('/health', HealthController::class)->name('admin.health'); });
     Route::prefix('admin')->middleware('permission:documents.view')->group(function () { Route::get('/documents', [DocumentController::class, 'index'])->name('admin.documents'); Route::get('/documents/{document}/download', [DocumentController::class, 'download'])->name('admin.documents.download'); });
     Route::prefix('admin')->middleware('permission:documents.manage')->group(function () { Route::post('/documents/folders', [DocumentController::class, 'storeFolder'])->name('admin.documents.folders.store'); Route::post('/documents', [DocumentController::class, 'store'])->name('admin.documents.store'); Route::post('/documents/chunks', [DocumentController::class, 'chunkUpload'])->name('admin.documents.chunks'); Route::delete('/documents/{document}', [DocumentController::class, 'destroy'])->name('admin.documents.destroy'); Route::post('/documents/folders/{folder}/rename', [DocumentController::class, 'renameFolder'])->name('admin.documents.folders.rename'); Route::post('/documents/folders/{folder}/move', [DocumentController::class, 'moveFolder'])->name('admin.documents.folders.move'); Route::post('/documents/folders/{folder}/copy', [DocumentController::class, 'copyFolder'])->name('admin.documents.folders.copy'); Route::delete('/documents/folders/{folder}', [DocumentController::class, 'destroyFolder'])->name('admin.documents.folders.destroy'); Route::post('/documents/{document}/rename', [DocumentController::class, 'rename'])->name('admin.documents.rename'); Route::post('/documents/{document}/move', [DocumentController::class, 'move'])->name('admin.documents.move'); Route::post('/documents/{document}/copy', [DocumentController::class, 'copy'])->name('admin.documents.copy'); });
     Route::prefix('admin')->middleware('permission:email.view')->group(function () { Route::get('/email', [InfrastructureController::class, 'email'])->name('admin.email'); });
