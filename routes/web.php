2ed002ccfae514d07131c765323e05e01bd1b416
Tarique Code <tariquecode@gmail.com>
2026-08-24 07:03:19 +0600

    Wire session authentication routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'login'])
+        ->middleware('throttle:6,1')
+        ->name('login.store');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', DashboardController::class)
@@ -19,4 +27,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/portal', DashboardController::class)
         ->name('portal.dashboard')
         ->middleware('role:client');
+
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
