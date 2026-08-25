9950c71658334259db4fd4391c020da28cf9bf4d
Tarique Code <tariquecode@gmail.com>
2026-08-25 09:58:37 +0600

    Configure the dedicated mail subdomain defaults

--- config/cpanel.php
+++ config/cpanel.php
@@ -6,8 +6,8 @@ return [
     'port' => (int) env('CPANEL_PORT', 2083),
     'username' => env('CPANEL_USERNAME'),
     'token' => env('CPANEL_API_TOKEN'),
-    'domain' => env('CPANEL_EMAIL_DOMAIN'),
+    'domain' => env('CPANEL_EMAIL_DOMAIN', 'fuelfreepowerplant.com'),
     'quota' => (int) env('CPANEL_EMAIL_QUOTA', 0), // 0 = unlimited in cPanel.
-    'mail_host' => env('CPANEL_MAIL_HOST'),
-    'webmail_url' => env('CPANEL_WEBMAIL_URL'),
+    'mail_host' => env('CPANEL_MAIL_HOST', 'mail.fuelfreepowerplant.com'),
+    'webmail_url' => env('CPANEL_WEBMAIL_URL', 'https://mail.fuelfreepowerplant.com'),
 ];
