ee203666aa381a02b1802a5f71e28ac80a46314a
Tarique Code <tariquecode@gmail.com>
2026-08-26 21:13:18 +0600

    Register webmail authentication middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\HomeAnnouncementPopup;
 use App\Http\Middleware\PermissionMiddleware;
 use App\Http\Middleware\RoleMiddleware;
 use App\Http\Middleware\SecurityHeaders;
+use App\Http\Middleware\WebmailAuth;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -21,6 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => RoleMiddleware::class,
             'permission' => PermissionMiddleware::class,
+            'webmail.auth' => WebmailAuth::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
