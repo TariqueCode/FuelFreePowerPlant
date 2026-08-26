7f5cab504304a9b336ae0cf6401ea16f27a5140d
Tarique Code <tariquecode@gmail.com>
2026-08-26 16:31:04 +0600

    Create shared Home footer for all public pages

---
+++ resources/views/partials/home-footer.blade.php
@@ -0,0 +1,2 @@
+<style>.footer{border-top:1px solid var(--line);padding:32px 0 50px;color:#688893;font-size:9px}</style>
+<footer class="footer"><div class="shell">© {{ date('Y') }} {{ $brand['name'] }} · All rights reserved.</div></footer>
