c5a8d43e445cacfa95578d60adbbeb6806680cbc
Tarique Code <tariquecode@gmail.com>
2026-08-26 21:17:54 +0600

    Make webmail message navigation host aware

--- resources/views/webmail/message.blade.php
+++ resources/views/webmail/message.blade.php
@@ -1,8 +1,6 @@
 @extends('webmail.layout')
 @section('content')
-<div class="hero"><div><div class="eyebrow">Message</div><h1 class="title">{{ $message['subject'] }}</h1></div><a class="btn" href="{{ route('webmail.inbox') }}">← Inbox</a></div>
-<article class="card reader">
-    <div class="reader-head"><h2>{{ $message['subject'] }}</h2><div class="meta"><strong>From:</strong> {{ $message['from'] }}<br><strong>To:</strong> {{ $message['to'] }}<br><strong>Date:</strong> {{ $message['date'] }}</div></div>
-    <div class="reader-body">{!! $message['body'] !!}</div>
-</article>
+@php($hostMode=request()->getHost()==='mail.fuelfreepowerplant.com') @php($base=$hostMode?'':'/webmail')
+<div class="hero"><div><div class="eyebrow">Message</div><h1 class="title">{{ $message['subject'] }}</h1></div><a class="btn" href="{{ url($base.'/inbox') }}">← Inbox</a></div>
+<article class="card reader"><div class="reader-head"><h2>{{ $message['subject'] }}</h2><div class="meta"><strong>From:</strong> {{ $message['from'] }}<br><strong>To:</strong> {{ $message['to'] }}<br><strong>Date:</strong> {{ $message['date'] }}</div></div><div class="reader-body">{!! $message['body'] !!}</div></article>
 @endsection
