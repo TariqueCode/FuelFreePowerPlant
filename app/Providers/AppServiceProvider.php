acad9af6c6a6233cfeb3a660873b55e1e842410f
Tarique Code <tariquecode@gmail.com>
2026-08-26 15:08:24 +0600

    Make configured company logo globally available

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
         if ($settings->has('company.domain')) config(['fuelfree.company.domain' => $settings['company.domain']]);
         if ($settings->has('company.tagline')) config(['fuelfree.company.tagline' => $settings['company.tagline']]);
         if ($settings->has('company.timezone')) config(['fuelfree.company.timezone' => $settings['company.timezone']]);
+        if ($settings->has('company.logo_path')) config(['fuelfree.company.logo_path' => $settings['company.logo_path']]);
         if ($settings->has('storage.quota_gib')) config(['fuelfree.storage.quota_bytes' => (int) round((float) $settings['storage.quota_gib'] * 1073741824)]);
     }
 }
