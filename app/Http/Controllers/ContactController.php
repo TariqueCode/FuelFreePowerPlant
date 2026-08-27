9ade44b5a2cdac0543e62c20b2e3b4120e76968e
Tarique Code <tariquecode@gmail.com>
2026-08-27 21:24:31 +0600

    Route website contact inquiries to configured info mailbox

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Inquiry;
+use App\Models\EmailAccount;
+use App\Services\WebmailService;
 use App\Models\SiteContentItem;
 use App\Models\SystemSetting;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Throwable;
 
 class ContactController extends Controller
 {
@@ -37,6 +40,22 @@ class ContactController extends Controller
 
         unset($data['website']);
         Inquiry::create($data);
+
+        $infoMailbox = EmailAccount::query()->where('address','info@fuelfreepowerplant.com')->where('status','active')->first();
+        if ($infoMailbox) {
+            try {
+                app(WebmailService::class)->send(
+                    $infoMailbox->address,
+                    $infoMailbox->password,
+                    $infoMailbox->address,
+                    $data['subject'],
+                    '<p><strong>New website inquiry</strong></p><p>Name: '.e($data['name']).'<br>Email: '.e($data['email']).'<br>Phone: '.e($data['phone'] ?: 'Not provided').'</p><p>'.nl2br(e($data['message'])).'</p>',
+                    ['imap_host'=>$infoMailbox->imap_host,'imap_port'=>$infoMailbox->imap_port,'smtp_host'=>$infoMailbox->smtp_host,'smtp_port'=>$infoMailbox->smtp_port]
+                );
+            } catch (Throwable $e) {
+                report($e);
+            }
+        }
 
         return back()->with('contact_status', 'Thank you. Your inquiry has been received and our team will contact you soon.');
     }
