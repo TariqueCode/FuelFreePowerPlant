7a4c068efd4ba3deee2fb08f5ae6ec92e0e94c29
Tarique Code <tariquecode@gmail.com>
2026-08-27 11:11:06 +0600

    Import cache facade for social link cache invalidation

--- app/Http/Controllers/Admin/SocialLinkController.php
+++ app/Http/Controllers/Admin/SocialLinkController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\SocialLink;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\View\View;
 
 class SocialLinkController extends Controller
