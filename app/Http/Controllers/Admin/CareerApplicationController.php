dbbd9df7e6601f27157683062c966c259da5730c
Tarique Code <tariquecode@gmail.com>
2026-08-29 13:52:07 +0600

    Restore Career controller view type import

--- app/Http/Controllers/Admin/CareerApplicationController.php
+++ app/Http/Controllers/Admin/CareerApplicationController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\CareerApplication;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\View\View;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\RedirectResponse;
 
