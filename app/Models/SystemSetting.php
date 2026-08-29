dfa7c04437fbd27e6ad817865e3873fe6810094e
Tarique Code <tariquecode@gmail.com>
2026-08-29 18:50:28 +0600

    Invalidate settings cache after changes

--- app/Models/SystemSetting.php
+++ app/Models/SystemSetting.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 class SystemSetting extends Model
 {
+    protected static function booted(): void
+    {
+        static::saved(fn () => Cache::forget('fuelfree.system_settings'));
+        static::deleted(fn () => Cache::forget('fuelfree.system_settings'));
+    }
     protected $fillable = ['key', 'value', 'is_sensitive'];
 
     protected $casts = ['is_sensitive' => 'boolean'];
