69a8dcb48f8579d742d5ff6c335ac190e7773148
Tarique Code <tariquecode@gmail.com>
2026-08-30 08:49:11 +0600

    Align scheduled upload cleanup with two-hour session expiry

--- routes/console.php
+++ routes/console.php
@@ -8,5 +8,5 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('fuel-free:cleanup-upload-sessions --hours=24')->hourly();
+Schedule::command('fuel-free:cleanup-upload-sessions --hours=2')->hourly();
 Schedule::command('fuel-free:sync-helpdesk-mail --limit=50')->everyMinute()->withoutOverlapping(5);
