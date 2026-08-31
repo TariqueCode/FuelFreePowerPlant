6b894790efbd334273d1fb268ceca96de9ebae5f
Tarique Code <tariquecode@gmail.com>
2026-08-31 20:08:22 +0600

    feat: add public resource attachment download

--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\SiteContentItem;
 use App\Models\SystemSetting;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\View\View;
 
 class ResourceController extends Controller
@@ -28,6 +30,23 @@ class ResourceController extends Controller
             ->paginate(12);
 
         return view('resources.index', ['resources' => $resources, 'brand' => $this->brand()]);
+    }
+
+    public function download(string $slug): Response
+    {
+        $resource = SiteContentItem::published()
+            ->whereIn('type', ['resource', 'resources'])
+            ->where('slug', $slug)
+            ->firstOrFail();
+
+        abort_unless($resource->attachment_path, 404);
+        abort_unless(Storage::disk('public')->exists($resource->attachment_path), 404);
+
+        return response()->download(
+            Storage::disk('public')->path($resource->attachment_path),
+            $resource->attachment_name ?: basename($resource->attachment_path),
+            ['Content-Type' => $resource->attachment_mime ?: 'application/pdf']
+        );
     }
 
     public function show(string $slug): View
