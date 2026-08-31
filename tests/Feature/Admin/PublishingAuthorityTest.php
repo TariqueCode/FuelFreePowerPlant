580bdf74ac408228ed57a867233da4f673741109
Tarique Code <tariquecode@gmail.com>
2026-08-31 17:31:20 +0600

    test: reuse seeded publishing permissions

--- tests/Feature/Admin/PublishingAuthorityTest.php
+++ tests/Feature/Admin/PublishingAuthorityTest.php
@@ -14,8 +14,8 @@ class PublishingAuthorityTest extends TestCase
 
     public function test_administrator_has_publish_authority_but_project_manager_does_not(): void
     {
-        $publish = Permission::create(['name' => 'Publish website content', 'slug' => 'website.publish']);
-        $websiteManage = Permission::create(['name' => 'Manage website sections', 'slug' => 'website.manage']);
+        $publish = Permission::firstOrCreate(['slug' => 'website.publish'], ['name' => 'Publish website content']);
+        $websiteManage = Permission::firstOrCreate(['slug' => 'website.manage'], ['name' => 'Manage website sections']);
 
         $administrator = Role::create(['name' => 'Administrator', 'slug' => 'administrator', 'is_system' => true]);
         $administrator->permissions()->sync([$publish->id, $websiteManage->id]);
@@ -35,8 +35,8 @@ class PublishingAuthorityTest extends TestCase
 
     public function test_cms_publishing_is_a_distinct_permission(): void
     {
-        $manage = Permission::create(['name' => 'Manage CMS', 'slug' => 'cms.manage']);
-        $publish = Permission::create(['name' => 'Publish CMS pages', 'slug' => 'cms.publish']);
+        $manage = Permission::firstOrCreate(['slug' => 'cms.manage'], ['name' => 'Manage CMS']);
+        $publish = Permission::firstOrCreate(['slug' => 'cms.publish'], ['name' => 'Publish CMS pages']);
 
         $role = Role::create(['name' => 'Content Editor', 'slug' => 'content-editor', 'is_system' => false]);
         $role->permissions()->sync([$manage->id]);
