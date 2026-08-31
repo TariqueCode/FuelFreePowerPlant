8e653231c4d0d239d163cf049bd470adf0dab503
Tarique Code <tariquecode@gmail.com>
2026-08-31 11:36:51 +0600

    feat: cast CMS framework flags

--- app/Models/CmsPage.php
+++ app/Models/CmsPage.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class CmsPage extends Model
 {
-    protected $fillable = ['title', 'slug', 'excerpt', 'content', 'is_published', 'meta_title', 'meta_description', 'builder_blocks', 'template'];
+    protected $fillable = ['title', 'slug', 'excerpt', 'content', 'is_published', 'meta_title', 'meta_description', 'builder_blocks', 'template', 'use_global_framework', 'use_global_header', 'use_global_footer'];
 
-    protected $casts = ['is_published' => 'boolean', 'builder_blocks' => 'array'];
+    protected $casts = ['is_published' => 'boolean', 'builder_blocks' => 'array', 'use_global_framework' => 'boolean', 'use_global_header' => 'boolean', 'use_global_footer' => 'boolean'];
 }
