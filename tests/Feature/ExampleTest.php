3e6610c805763b00a7afe48f4d2efa2e1798e311
Tarique Code <tariquecode@gmail.com>
2026-09-21 15:02:20 +0600

    Add sitemap availability regression coverage

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,17 @@ class ExampleTest extends TestCase
         $this->get('/')->assertSuccessful();
     }
 
+
+    public function test_public_sitemap_is_available_and_xml(): void
+    {
+        $response = $this->get('/sitemap.xml');
+
+        $response->assertOk();
+        $response->assertHeader('Content-Type', 'application/xml; charset=UTF-8');
+        $this->assertStringContainsString('<urlset', $response->getContent());
+        $this->assertStringContainsString('/about-us', $response->getContent());
+    }
+
     public function test_public_career_page_is_available(): void
     {
         $response = $this->get('/career');
